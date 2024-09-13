088b2d493462fe0706e9a5f1962b3f9949caa5ac
Venkatesh4305 <venkateshallu809@gmail.com>
2024-09-13 15:31:38 +0530

    joi

---
+++ 2.js
@@ -0,0 +1,7 @@
+let person = { name: "jyothirmayudu", marks: 20 };
+console.log(person);
+person.marks = 30;
+console.log(person);
+let colors = ["red", "blue"];
+colors[1] = "yellow";
+console.log(colors);
